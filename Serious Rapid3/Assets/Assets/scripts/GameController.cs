ac5225dd5d1f00c8e6b4754a298e1eb2fd3157bf
miiicom <miiicom1230@gmail.com>
2017-10-23 11:22:16 -0600

    update

---
+++ Serious Rapid3/Assets/Assets/scripts/GameController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Text questionText;
+	public SimpleObjectPool answerButtonObjectPool;
+	public Transform answerButtonParent;
+
+	private DataController dataController;
+	private RoundData currentRoundData;
+	private QuestionData[] questionPool;
+
+	private bool isRoundAactive;
+	private float timeRemaining;
+	private int questionIndex;
+	private int playScore;
+	private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		dataController = FindObjectOfType<DataController> ();
+		currentRoundData = dataController.GetCurrentRoundData ();
+		questionPool = currentRoundData.questions;
+		timeRemaining = currentRoundData.timeLimitInSeconds;
+
+		playScore = 0;
+		questionIndex = 0;
+
+		ShowQuestion ();
+		isRoundAactive = true;
+	}
+
+	private void ShowQuestion()
+	{
+		RemoveAanswerButtons ();
+		QuestionData questionData = questionPool [questionIndex];
+		questionText.text = questionData.questionText;
+
+		for (int i = 0; i < questionData.answers.Length; i++) 
+		{
+			GameObject answerButtonGameObjetc = answerButtonObjectPool.GetObject ();
+			answerButtonGameObjetc.transform.SetParent (answerButtonParent);
+
+			AnswerButton answerButton = answerButtonGameObjetc.GetComponent<AnswerButton> ();
+			answerButton.Setup (questionData.answers [i]);
+		}
+
+	}
+
+	private void RemoveAanswerButtons()
+	{
+		while (answerButtonGameObjects.Count > 0) 
+		{
+			answerButtonObjectPool.ReturnObject (answerButtonGameObjects [0]);
+			answerButtonGameObjects.RemoveAt (0);
+		}
+			
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Serious Rapid3/Assets/Assets/scripts/QuestionController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class QuestionController : MonoBehaviour {
+
+	public void StartQuestion()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
